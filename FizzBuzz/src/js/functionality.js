16be11c6de753b50246f1198158338cc83a403dd
varlackc <varlackc@mail.gvsu.edu>
2020-08-16 10:57:34 -0500

    Update functionality.js

--- FizzBuzz/src/js/functionality.js
+++ FizzBuzz/src/js/functionality.js
@@ -5,29 +5,32 @@
 var message = "";
 var fizzValue = 2;
 var buzzValue = 3;
+var result = "";
 
-for (var i = 1; i <= 100; i++) {
-
-    //Check to see if Fizz value should be outputed
-    if (i % fizzValue == 0) {
-        message = "Fizz";
-    }
-    //Check to see if Buzz value should be outputed
-    if (i % buzzValue == 0) {
-        message += "Buzz";
-    }
-
-    //output the result
-    console.log(i + "\t" + message);
-
-    //reset the message value
-    message = "";
-
-    $(document).ready(function() {
+$(document).ready(function() {
+    $('#submitButton').click(function() {
         // Code to display values
-        $('#submitButton').click(function() {
-            // JavaScript 
-            $('#details').html(message);
-        });
+        for (var i = 1; i <= 100; i++) {
+
+            //Check to see if Fizz value should be outputed
+            if (i % fizzValue == 0) {
+                message = " - Fizz";
+            }
+            //Check to see if Buzz value should be outputed
+            if (i % buzzValue == 0) {
+                message += " - Buzz";
+            }
+
+            //output the result
+            console.log(i + "\t" + message);
+            result += (i + message + "<br/>");
+
+            //reset the message value
+            message = "";
+        }
+        result += "<br/>";
+
+        // JavaScript 
+        $('#details').html(result);
     });
-}
+});
