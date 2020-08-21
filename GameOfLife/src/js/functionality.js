0f517c53cd19eae4f91c6065c617d2ee36fc27dd
varlackc <varlackc@mail.gvsu.edu>
2020-08-20 23:11:12 -0500

    Update functionality.js

--- GameOfLife/src/js/functionality.js
+++ GameOfLife/src/js/functionality.js
@@ -9,29 +9,9 @@ $(document).ready(function() {
 
         //Declare Variables
         var message = "";
-        var fizzValue = parseInt($('#fizzValue').val());
-        var buzzValue = parseInt($('#buzzValue').val());
-        var startValue = parseInt($('#startingValue').val());
-        var endValue = parseInt($('#endingValue').val());
         var result = "";
 
-        console.log("Fizz Value: " + fizzValue);
-        console.log("Buzz Value" + buzzValue);
-
-        // Loop to improve
-        for (var i = startValue; i <= endValue; i++) {
-            //Check to see if Fizz value should be outputed
-            if (i % fizzValue == 0) {
-                message = " - Fizz";
-            }
-            //Check to see if Buzz value should be outputed
-            if (i % buzzValue == 0) {
-                message += " - Buzz";
-            }
-            result += ("<p><span Class='textPad'>" + i + message + "</span></p>"); //output the result
-            message = ""; //reset the message value
-        }
-        result += "<br/>";
+        console.log("Game of Life Test");
 
         // JavaScript 
         $('#details').html(result);
