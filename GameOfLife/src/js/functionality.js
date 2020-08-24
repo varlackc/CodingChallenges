8c90079e1707ef9f7c2a0b22b931d5f493301c51
varlackc <varlackc@mail.gvsu.edu>
2020-08-23 23:42:27 -0500

    Update functionality.js

--- GameOfLife/src/js/functionality.js
+++ GameOfLife/src/js/functionality.js
@@ -8,7 +8,76 @@
     4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.  
 */
 
+// Declare Global Variables
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const size = 800;
+const scale = 8;
+const resolution = size / scale;
 
+let cells;
+
+setup();
+randomCells();
+
+function setup() {
+    canvas.width = size;
+    canvas.height = size;
+    context.scale(scale, scale);
+    context.fillStyle = "black";
+}
+
+function createCells() {
+    let arr = new Array(resolution);
+    for (let x = 0; x < resolution; x++) {
+        let rows = new Array(resolution);
+        for (let y = 0; y < resolution; y++) {
+            cols[y] = false;
+        }
+        arr[x] = cols;
+    }
+    return arr;
+}
+
+function randomCells() {
+    for (let y = 0; y < resolution; y++) {
+        for (let x = 0; x < resolution; x++) {
+            if (Math.random() < 0.5) cells[x][y] = true;;
+        }
+    }
+}
+
+function drawCells() {
+    for (let)
+}
+
+/*
+
+function make2DArray(cols, rows) {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+
+// Global variables
+let grid;
+let cols = 10;
+let rows = 10;
+
+function setup() {
+
+    grid = make2DArray(10, 10);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            // Randomly populate the cells
+            grid[i][j] = floor(random(2));
+        }
+    }
+
+}
 
 /*
 $(document).ready(function() {
