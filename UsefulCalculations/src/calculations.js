ce6485341a42bcb94e542818503a2fe66ba17a1f
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-09-07 22:42:42 -0700

    Update calculations.js

--- UsefulCalculations/src/calculations.js
+++ UsefulCalculations/src/calculations.js
@@ -1,5 +1,5 @@
 const ListItem = props => {
-    return React.createElement('li', {className: 'listItem'}, props.title);
+    return React.createElement('li', {className: 'list-Item', onClick: props.onDelete}, props.title);
 }
 
 class List extends React.Component {
@@ -8,13 +8,30 @@ class List extends React.Component {
         this.state = {items: ['Squares', 'Rectangles', 'circles']};
     }
 
+    //Add new elementes
+    addItemHandler(){
+        this.setState((prevState) => {
+            return {items: prevState.items.concat("Triangle")}
+        });
+    }
+
+    // delete item
+    deleteItemHandler(text){
+        this.setState(prevState => {
+            return {items: prevState.items.filter(item => {
+                return item !== text;
+            }) };
+        });
+    }
+
     render() {
         return React.createElement('div', null, [
-            React.createElement('ul', null, this.state.items.map(item => {
-                return React.createElement(ListItem, {title: item});
-            }))
+            React.createElement('ul', {key: 'geometry-list'}, this.state.items.map(item => {
+                return React.createElement(ListItem, {title: item, key: item, onDelete: this.deleteItemHandler.bind(this, item)});
+            })), 
+            React.createElement('button', {key:'geometry-list', onClick: this.addItemHandler.bind(this)}, 'Add Geometry')
         ]);
     }
 }
 
-ReactDOM.render(?React.createElement(List), document.getElementById("geometry-list"));
+ReactDOM.render(React.createElement(List), document.getElementById("geometry-list"));
