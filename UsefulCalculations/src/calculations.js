0a9bd7543399a92c013c8089266a3ed2a0191175
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-09-07 22:00:57 -0700

    Update calculations.js

---
+++ UsefulCalculations/src/calculations.js
@@ -0,0 +1,16 @@
+const ListItem = props => {
+    return React.createElement('li', {className: 'listItem'}, props.title);
+}
+
+class List extends React.Component {
+    constructor(){
+        super();
+        this.state = {items: ['Squares', 'Rectangles', 'circles']};
+    }
+
+    render() {
+        return React.createElement('div', null, [
+            React.createElement('ul', null,)
+        ]);
+    }
+}
