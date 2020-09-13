cc4808121fe4e8147ffb7e5a7efe3882b6af7172
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-09-12 21:46:34 -0700

    Create ReactTest.js

---
+++ UsefulCalculations/src/ReactTest.js
@@ -0,0 +1,122 @@
+const ListItem = props => {
+    return React.createElement('li', {className: 'list-Item', onClick: props.onDelete}, props.title);
+}
+
+class List extends React.Component {
+    constructor(){
+        super();
+        this.state = {items: ['Squares', 'Rectangles', 'circles']};
+    }
+
+    //Add new elementes
+    addItemHandler(){
+        this.setState((prevState) => {
+            return {items: prevState.items.concat("Triangle")}
+        });
+    }
+
+    // delete item
+    deleteItemHandler(text){
+        this.setState(prevState => {
+            return {items: prevState.items.filter(item => {
+                return item !== text;
+            }) };
+        });
+    }
+
+    render() {
+        return React.createElement('div', null, [
+            React.createElement('ul', {key: 'geometry-list'}, this.state.items.map(item => {
+                return React.createElement(ListItem, {title: item, key: item, onDelete: this.deleteItemHandler.bind(this, item)});
+            })), 
+            React.createElement('button', {key:'geometry-list', onClick: this.addItemHandler.bind(this)}, 'Add Geometry')
+        ]);
+    }
+}
+
+ReactDOM.render(React.createElement(List), document.getElementById("geometry-list"));
+
+// Mathematical Calculations
+
+// Area of a rectangle
+function RectangleArea(width, height)
+{
+    var area = width * height;
+    return area;
+}
+
+// Perimeter of rectangle
+function RectanglePerimeter(width, height)
+{
+    var perimeter = (2*width) + (2*height);
+    return perimeter;
+}
+
+// Triangle Area
+function TriangleArea(base, height)
+{
+    var area = (base*height)/2;
+    return area;
+}
+
+// Triangle Perimeter
+function TrianglePerimeter(base, side1, side2){
+    var perimeter = base + side1 + side2;
+    return perimeter;
+}
+
+// Circle Area
+function CircleArea(radius)
+{
+    var area = (radius*radius*Math.PI);
+    return area;
+}
+
+// Circle Perimeter
+function CirclePerimeter(radius){
+    var perimeter = (2*radius*Math.PI);
+    return perimeter;
+}
+
+/**
+ * List of usefull calculations
+ * 
+ * Area of a rectangle
+ * 
+ * perimeter of a rectangle
+ * 
+ * Area of a triangle
+ * 
+ * Perimeter of a triangle
+ * 
+ * Area of a circle
+ * 
+ * Perimeter of a circle
+ * 
+ * surface area of a box
+ * 
+ * volume of a box
+ * 
+ * surface area of a sphere, 
+ * 
+ * volume of a sphere
+ * 
+ * Surface area of a cylinder
+ * 
+ * volume of a cylinder
+ * 
+ * Intercepts of a linear equation
+ * 
+ * Auadratic formula, find vertex, quadratic regression
+ * 
+ * Exponential functions
+ * 
+ * Logarithmic functions
+ * 
+ * Exponential equations
+ * 
+ * Non linear modelx
+ * 
+ * syustems of linear equations
+ * 
+ */
