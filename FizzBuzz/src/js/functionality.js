96fefa660b854ba9a1024fd1e4e39e657258c0b5
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-10-04 22:37:08 -0700

    Update functionality.js

--- FizzBuzz/src/js/functionality.js
+++ FizzBuzz/src/js/functionality.js
@@ -1,10 +1,8 @@
-/*
+/**
     Fizz Buzz Challenge
 */
 
 $(document).ready(function() {
-
-
     $('#submitButton').click(function() {
 
         //Declare Variables
@@ -14,10 +12,8 @@ $(document).ready(function() {
         var startValue = parseInt($('#startingValue').val());
         var endValue = parseInt($('#endingValue').val());
         var result = "";
-
         console.log("Fizz Value: " + fizzValue);
         console.log("Buzz Value" + buzzValue);
-
         // Loop to process the different numbers
         for (var i = startValue; i <= endValue; i++) {
             //Check to see if Fizz value should be outputed
@@ -36,7 +32,6 @@ $(document).ready(function() {
         // Output the result to the browser 
         $('#details').html(result);
     });
-
     // Clear the screen
     $('#clearButton').click(function() {
         $('#details').html(" ");
