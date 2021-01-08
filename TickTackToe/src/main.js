8f6b5e41d083959dc3b3d4b5b3a3600849fcb59b
Maxwell Varlack <varlackc@mail.gvsu.edu>
2021-01-07 22:15:52 -0800

    Create multidimensional array
    Create multidimensional array to keep track of the board status

--- TickTackToe/src/main.js
+++ TickTackToe/src/main.js
@@ -3,8 +3,48 @@ var board = [['','',''], ['','',''], ['','','']]
 
 function clickEvent(id){
     //update the front end
-    document.getElementById(id).innerHTML = "X";
+    var stamp = "X";
+    document.getElementById(id).innerHTML = stamp;
     
     //update the board values
-    //switch{}
+    switch(id)
+    {
+        case "topLeft":
+            board[0][0] = stamp;
+        break;
+        
+        case "topCenter":
+            board[0][1] = stamp;
+        break;
+        
+        case "topRight":
+            board[0][2] = stamp;
+        break;
+        
+        case "midLeft":
+            board[1][0] = stamp;
+        break;
+        
+        case "midCenter":
+            board[1][1] = stamp;
+        break;
+        
+        case "midRight":
+            board[1][2] = stamp;
+        break;
+        
+        case "bottomLeft":
+            board[2][0] = stamp;
+        break;
+        
+        case "bottomCenter":
+            board[2][1] = stamp;
+        break;
+        
+        case "bottomRight":
+            board[2][2] = stamp;
+        break;
+    }
+    //display result
+    console.log(board);
 };
